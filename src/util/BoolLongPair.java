5952ded7d18dc2ade43912555dc9477a256b5ae8
Alexis Espinoza <alexis.espinoza@ug.uchile.cl>
2019-07-08 02:43:47 -0400

    Added Javadoc to BoolLongPair

--- src/util/BoolLongPair.java
+++ src/util/BoolLongPair.java
@@ -5,15 +5,31 @@ public class BoolLongPair {
 	private final boolean b;
 	private final long l;
 
+	/**
+	 * A simple pair containing a boolean and a long.
+	 *
+	 * @param b The boolean in the pair.
+	 * @param l The long in the pair.
+	 */
 	public BoolLongPair(boolean b, long l) {
 		this.b = b;
 		this.l = l;
 	}
 
+	/**
+	 * Returns the boolean in the pair.
+	 *
+	 * @return The boolean in the pair.
+	 */
 	public boolean getBool() {
 		return this.b;
 	}
 
+	/**
+	 * Returns the long in the pair.
+	 *
+	 * @return The long in the pair.
+	 */
 	public long getLong() {
 		return this.l;
 	}
